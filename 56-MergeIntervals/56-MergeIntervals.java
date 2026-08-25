5c0ff5fa56a3e39abb182fa3caace6f9d6fdcd3e
Hitika Chhillar <hitikachhillar6@gmail.com>
2026-08-25 23:53:08 +0530

    Create 56-MergeIntervals

---
+++ 56-MergeIntervals/56-MergeIntervals.java
@@ -0,0 +1,25 @@
+// Last updated: 8/25/2026, 11:53:10 PM
+class Solution {
+    public int[][] merge(int[][] intervals) {
+
+        // Sort intervals based on start time
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+
+        List<int[]> ans = new ArrayList<>();
+
+        for (int[] interval : intervals) {
+
+            // If list is empty OR no overlap
+            if (ans.isEmpty() || ans.get(ans.size() - 1)[1] < interval[0]) {
+                ans.add(interval);
+            }
+            // Overlap exists -> merge
+            else {
+                ans.get(ans.size() - 1)[1] =
+                        Math.max(ans.get(ans.size() - 1)[1], interval[1]);
+            }
+        }
+
+        return ans.toArray(new int[ans.size()][]);
+     }
+}
