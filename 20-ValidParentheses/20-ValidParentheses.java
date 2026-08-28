172a78913b23a2dfba57688b65b748863f774d71
Hitika Chhillar <hitikachhillar6@gmail.com>
2026-08-28 17:45:12 +0530

    Create 20-ValidParentheses

---
+++ 20-ValidParentheses/20-ValidParentheses.java
@@ -0,0 +1,30 @@
+// Last updated: 8/28/2026, 5:45:12 PM
+class Solution {
+    public boolean isValid(String s) {
+
+        Stack<Character> stack = new Stack<>();
+
+        for(char ch : s.toCharArray()){
+
+            if(ch == '(' || ch == '{' || ch == '['){
+                stack.push(ch);
+            }
+            else{
+                if(stack.isEmpty()){
+                    return false;
+                }
+
+                char top = stack.pop();
+
+                if((ch ==')' && top != '(') ||
+                    (ch =='}' && top != '{')||
+                    (ch ==']' && top != '[')){
+
+                        return false;
+                    }
+            }
+
+        }
+        return stack.isEmpty();
+    }
+}
